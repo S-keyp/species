e5cc1cf3fb27879b3c217102c2dbf4229b8b785e
S-Keyp <s-keyp@outlook.fr>
2023-11-08 16:59:23 +0100

    feat: comment everything in main for test

--- src/main/java/com/example/species/SpeciesApplication.java
+++ src/main/java/com/example/species/SpeciesApplication.java
@@ -1,24 +1,17 @@
+
 package com.example.species;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.species.enums.Sex;
-import com.example.species.model.Person;
-import com.example.species.model.Species;
 import com.example.species.repository.AnimalRepository;
 import com.example.species.repository.PersonRepository;
 import com.example.species.repository.SpeciesRepository;
 
 @SpringBootApplication
-public class SpeciesApplication implements CommandLineRunner {
+public class SpeciesApplication { // implements CommandLineRunner
 
 	@Autowired
 	private AnimalRepository animalRepo;
@@ -31,39 +24,39 @@ public class SpeciesApplication implements CommandLineRunner {
 		SpringApplication.run(SpeciesApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
+	// @Override
+	// public void run(String... args) throws Exception {
 		
 		// DELETE (RIP Henry?)
-		System.out.println("%d".formatted(personRepo.findAll().size()));
+		// System.out.println("%d".formatted(personRepo.findAll().size()));
 		
-		Person person = new Person();
-		person.setAge(22);
-		person.setId(1);
-		person.setFirstName("Henry");
-		person.setLastName("Lamarque");
-		personRepo.delete(person);
+		// Person person = new Person();
+		// person.setAge(22);
+		// person.setId(1);
+		// person.setFirstName("Henry");
+		// person.setLastName("Lamarque");
+		// personRepo.delete(person);
 		
-		System.out.println("%d".formatted(personRepo.findAll().size()));
+		// System.out.println("%d".formatted(personRepo.findAll().size()));
 		
 		// CREATE (Revive Henry?)
-		Person person2 = new Person();
-		person2.setAge(22);
-		person2.setId(1);
-		person2.setFirstName("Henry");
-		person2.setLastName("Lamarque");
-		personRepo.save(person2);
+		// Person person2 = new Person();
+		// person2.setAge(22);
+		// person2.setId(1);
+		// person2.setFirstName("Henry");
+		// person2.setLastName("Lamarque");
+		// personRepo.save(person2);
 
 		// SELECT
-		Optional<Person> person3 = personRepo.findById(1);
-		if(person3.isPresent()) {
-			System.out.println("Personne trouvé, c'est: %s".formatted(person3.get()));
-			person3.get().setFirstName("Plus Henry");
-			personRepo.save(person3.get());
-			System.out.println("Personne updated");
-		} else {
-			System.err.println("Personne non trouvée");
-		}
+		// Optional<Person> person3 = personRepo.findById(1);
+		// if(person3.isPresent()) {
+		// 	System.out.println("Personne trouvé, c'est: %s".formatted(person3.get()));
+		// 	person3.get().setFirstName("Plus Henry");
+		// 	personRepo.save(person3.get());
+		// 	System.out.println("Personne updated");
+		// } else {
+		// 	System.err.println("Personne non trouvée");
+		// }
 		
 
 		// READ all persons
@@ -137,11 +130,11 @@ public class SpeciesApplication implements CommandLineRunner {
 		// System.out.println(personRepo.findAllByAgeBetween(32, 50));
 		
 		// ANIMAL REPO
-		System.out.println("-------------------------------");
-		System.out.println("ANIMAL");
-		System.out.println("-------------------------------");
+		// System.out.println("-------------------------------");
+		// System.out.println("ANIMAL");
+		// System.out.println("-------------------------------");
 		
-		System.out.println(animalRepo.getQuantityBySex(Sex.M));
+		// System.out.println(animalRepo.getQuantityBySex(Sex.M));
 
 
 
@@ -162,13 +155,13 @@ public class SpeciesApplication implements CommandLineRunner {
 		// System.out.println("-------------------------------");
 		// System.out.println("-------------------------------");
 		// System.out.println("");
-		personRepo.deletePersonsWithOutAnimal();
-		System.out.println(personRepo.createPerson("Oink oink"));
-		System.out.println(personRepo.bulkCreateMiguelPerson(10));
+		// personRepo.deletePersonsWithOutAnimal();
+		// System.out.println(personRepo.createPerson("Oink oink"));
+		// System.out.println(personRepo.bulkCreateMiguelPerson(10));
 
 
 
-	}
+	// }
 
 	@GetMapping("hello")
 	public String hello() {
